0df8dd5fe49a41808ced8fdbe9f398224c66f4f5
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-07-13 11:52:26 +0530

    construct binay tree

---
+++ constructBinaryTree.py
@@ -0,0 +1,26 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+def sortedArrayToBST( nums):
+    if(not nums):
+        return None
+    n=len(nums)
+    mid=n//2
+    root=TreeNode(nums[mid])
+    root.left=sortedArrayToBST(nums[:mid])
+    root.right=sortedArrayToBST(nums[mid+1:])
+    return root
+def preOrder(node):
+    if not node:
+        return
+     
+    print(node.val)
+    preOrder(node.left)
+    preOrder(node.right)
+l=[-10,-3,0,5,9]
+root=sortedArrayToBST(l)
+preOrder(root)
+
+    
