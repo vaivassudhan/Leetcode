1727f1fee0a91b6a18fbeb3809678d52913da28f
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-07-19 22:30:55 +0530

    bubble sort

---
+++ Sorting/bubbleSort.java
@@ -0,0 +1,29 @@
+public class bubbleSort {
+
+    public void bsort(int a[])
+    {
+        int n=a.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    a[j+1]=a[j]+a[j+1];
+                    a[j]=a[j+1]-a[j];
+                    a[j+1]=a[j+1]-a[j];
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+    }
+    public static void main(String args[])
+    {
+        int a[]={1,5,4,6,3,7,2};
+        bubbleSort bs = new bubbleSort();
+        bs.bsort(a);
+    }
+}
